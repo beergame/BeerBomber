07776f45a9b5aa1becf22640c53a1152594d1b4c
Nandotk <vic.130@hotmail.fr>
2017-04-02 21:29:49 +0200

    add bomb implement

---
+++ src/bomb.c
@@ -0,0 +1,29 @@
+#include "bomb.h"
+
+extern void drawStandardEntity(void);
+
+extern SDL_Surface *getSprite(int);
+
+static void fireStandardBomb(void);
+
+void addBomb(t_case_map **map, int x, int y)
+{
+    map[x][y].entity.x = x;
+    map[x][y].entity.y = y;
+    map[x][y].entity.action = &fireStandardBomb;
+    map[x][y].entity.draw = &drawStandardEntity;
+    map[x][y].entity.sprite = getSprite(BOMB_SPRITE);
+    map[x][y].entity.type = 0;
+}
+
+static void fireStandardBomb()
+{
+    self->type++;
+
+    if (self->type >= 100)
+        self->sprite = getSprite(BOMB_SPRITE2);
+    if (self->type >= 200)
+        self->sprite = getSprite(BOMB_SPRITE3);
+    if (self->type >= 280)
+        self->sprite = getSprite(MAP_SPRITE_FIRE);
+}
