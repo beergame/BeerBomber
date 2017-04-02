adccf8e6763c1281901643328e59bde781d6f2b8
Nandotk <vic.130@hotmail.fr>
2017-04-02 21:30:35 +0200

    add player and struct

---
+++ src/player.c
@@ -0,0 +1,77 @@
+#include "player.h"
+
+extern int loadSprite(char *);
+
+extern void drawImage(SDL_Surface *, int, int);
+
+extern void addBomb(t_case_map**, int, int);
+
+extern SDL_Surface *getSprite(int);
+
+void initPlayer(t_case_map **map, int x, int y)
+{
+    map[x][y].entity.sprite = getSprite(PLAYER_SPRITE1);
+    map[x][y].entity.active = 1;
+
+    map[x][y].entity.x = SCREEN_WIDTH / 2;
+    map[x][y].entity.y = SCREEN_HEIGHT / 2;
+}
+
+void doPlayer(t_case_map **map, int x, int y)
+{
+    map[x][y].entity.thinkTime--;
+
+    if (map[x][y].entity.thinkTime <= 0) {
+        map[x][y].entity.thinkTime = 0;
+    }
+
+    if (input.up == 1) {
+        map[x][y].entity.y -= PLAYER_SPEED;
+
+        /* Don't allow the player to move off the screen */
+
+        if (map[x][y].entity.y < 0) {
+            map[x][y].entity.y = 0;
+        }
+    }
+
+    if (input.down == 1) {
+        map[x][y].entity.y += PLAYER_SPEED;
+
+        /* Don't allow the player to move off the screen */
+
+        if (map[x][y].entity.y + map[x][y].entity.sprite->h >= SCREEN_HEIGHT) {
+            map[x][y].entity.y = SCREEN_HEIGHT - (map[x][y].entity.sprite->h + 1);
+        }
+    }
+
+    if (input.left == 1) {
+        map[x][y].entity.x -= PLAYER_SPEED;
+
+        /* Don't allow the player to move off the screen */
+
+        if (map[x][y].entity.x < 0) {
+            map[x][y].entity.x = 0;
+        }
+    }
+
+    if (input.right == 1) {
+        map[x][y].entity.x += PLAYER_SPEED;
+
+        /* Don't allow the player to move off the screen */
+
+        if (map[x][y].entity.x + map[x][y].entity.sprite->w >= SCREEN_WIDTH) {
+            map[x][y].entity.x = SCREEN_WIDTH - (map[x][y].entity.sprite->w + 1);
+        }
+    }
+
+    if (input.fire == 1) {
+        /* You can only fire when the thinkTime is 0 or less */
+
+        if (map[x][y].entity.thinkTime <= 0) {
+            addBomb(map, map[x][y].entity.x, map[x][y].entity.y);
+
+            map[x][y].entity.thinkTime = MAX_RELOAD_TIME;
+        }
+    }
+}
---
+++ src/structs.h
@@ -0,0 +1,51 @@
+#include "defs.h"
+
+typedef struct Entity
+{
+    int active, type;
+    int x, y, thinkTime;
+    SDL_Surface *sprite;
+
+    void (*action)(void);
+
+    void (*draw)(void);
+} Entity;
+
+typedef struct s_case_map
+{
+    int inFire;
+    int hasBomb;
+    SDL_Surface *sprite;
+    Entity entity;
+    unsigned int mapType;
+} t_case_map;
+
+typedef struct Game
+{
+    int score, status;
+    t_case_map **map;
+    SDL_Surface *screen;
+    TTF_Font *font;
+    SDL_Joystick *joystick;
+} Game;
+
+typedef struct Sprite
+{
+    SDL_Surface *image;
+} Sprite;
+
+typedef struct Sound
+{
+    Mix_Chunk *effect;
+} Sound;
+
+typedef struct Control
+{
+    int up, down, left, right, fire;
+} Control;
+
+typedef struct Redefine
+{
+    int redefineIndex;
+    char redefineString[255];
+} Redefine;
