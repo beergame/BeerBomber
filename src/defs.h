467ef5e1f87b0ed86cfa0cb9067b1de73804ec0a
victorbalssa <victor.balssa@gmail.com>
2022-06-27 15:51:34 -0700

    Revert "add windows compatibility"
    This reverts commit 853c0abbddc8d50f378b63e4fb2b7f979742dece.

--- src/defs.h
+++ src/defs.h
@@ -1,34 +1,14 @@
 #ifndef __DEFS_H__
 # define __DEFS_H__
 
-#ifdef      __WIN32__
-#define         _BSD_SOURCE
-#ifndef         _WIN32_WINNT
-#define         _WIN32_WINNT 0x0501
-#endif
-
-#include        <winsock2.h>
-#include        <winsock.h>
-#include        <windows.h>
-#include        <ws2tcpip.h>
-#include        <stdio.h>
-#else
-
-# include <arpa/inet.h>
-# include <sys/socket.h>
-# include <sys/select.h>
-# include <netdb.h>
-#endif
-
 # include <stdio.h>
 # include <string.h>
 # include <time.h>
 # include <stdlib.h>
-
-
-
-
-
+# include <arpa/inet.h>
+# include <sys/socket.h>
+# include <sys/select.h>
+# include <netdb.h>
 # include <pthread.h>
 # include "SDL2/SDL.h"
 # include "SDL2/SDL_image.h"
